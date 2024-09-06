676b2ac4a70be5c1932a709dbe31e80cdc52a120
Amr Okasha <157937738+Amr546214@users.noreply.github.com>
2024-09-06 10:44:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function amr() {
+    document.getElementById("area").innerHTML =
+        "<p style='font-size:70px;'>NAME: Amr Mahmoud Yehia</p>" +
+        "<p style='font-size:70px;'>AGE: 20</p>" +
+        "<p style='font-size:70px;'>LEVEL: 2</p>" +
+        "<p style='font-size:70px;'>UNIVERSITY: EELU</p>"+
+        "<p style='font-size:70px;'>ID: 2200890</p>"
+} 
+
+
